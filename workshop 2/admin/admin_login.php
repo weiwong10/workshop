c88d9f4d4fe34a77e68f3065a0e069357de97ffc
azuranatasha <121167028+azuranatasha@users.noreply.github.com>
2023-01-03 00:36:25 +0800

    Update admin_login.php

--- workshop 2/admin/admin_login.php
+++ workshop 2/admin/admin_login.php
@@ -4,9 +4,16 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Admin Main Page</title>
+
+	<link rel="stylesheet" type="text/css" href="admin_style1.css">
 </head>
+
 <body>
-	<h1>Admin</h1>
+
+		<div>
+		<?php include("nav_admin.php")?>
+		</div>
+		<h1>Home Page</h1>
 
 </body>
 </html>

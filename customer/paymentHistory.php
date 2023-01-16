329872cfc47533a0adb2b29e4423dae23854bab9
weiwong10 <weiwong990827@gmail.com>
2023-01-16 13:31:48 +0800

    latest

--- customer/paymentHistory.php
+++ customer/paymentHistory.php
@@ -1,5 +1,9 @@
 <?php
 include "../connect.php";
+include("nav/nav_payment.php");
+session_start();
+$username = $_SESSION['username'];
+
 
 ?>
 <!DOCTYPE html>
@@ -11,42 +15,83 @@ include "../connect.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
- <div>
 
-<table class="table table-light table-striped">
-  <thead>
-    <tr>
-      <th scope="col">Payment ID</th>
-      <th scope="col">Payment Date</th>
-      <th scope="col">Amount</th>
-    </tr>
-  </thead>
-  <tbody>
+<div class = "container">
 
+  <h1 style="margin-left:40% ;margin-top:80px"   class="">Payment History</h1>
+     </div>
+		
+		<meta charset = "utf-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+		<link rel = "stylesheet" type = "text/css" href = "../css/bootstrap.css " />
+		<link rel = "stylesheet" type = "text/css" href = "../css/style.css" />
 
-    <?php
+	<br />
+	<div class = "container-fluid">
+		<div class = "panel panel-default">
+			<div class = "panel-body">
+				<!--<div class = "alert alert-info">Manage Admin</div>-->
+				<br />
+				<br />
+				<table id = "table" class = "table table-bordered">
+					<thead>
+						<tr>
+							<th><center>trip ID</th>
+        					<th><center>Trip Title</th>
+        					<th><center>Date</th>
+        					<th><center>Amount (RM)</th>
+        						
+						</tr>
+					</thead>
+					<tbody>
+					<?php
+				
 
-    $sql = "Select paymentID,payment_date,amount from payment";
-    $result = mysqli_query($conn,$sql);
-    if($result){
-        while($row=mysqli_fetch_assoc($result)){
-            $paymentID = $row['paymentID'];
-            $payment_date = $row['payment_date'];
-            $amount = $row['amount'];
-            echo '<tr>
-            <th scope="row">'.$paymentID.'</th>
-            <td>'.$payment_date.'</td>
-            <td>' .$amount.'</td>
-            <td>
-            </td>
-          </tr>';
-        }
-    }
+						$query = $conn->query("SELECT tripID, title, payment_date, amount from trip t, payment p WHERE t.paymentID = p.paymentID AND username = '$username'") or die(mysqli_error());
+						while($fetch = $query->fetch_array()){
+					?>	
+						<tr>
+						
+							<td><center><?php echo $fetch['tripID']?></td>
+								<td><center><?php echo $fetch['title']?></td>
+									<td><center><?php echo $fetch['payment_date']?></td>
+										<td><center><?php echo $fetch['amount']?></td>
+							
+							
 
-    
-    ?>
-  </tbody>
-</table>
-</div>
+						</tr>
+					<?php
+						}
+					?>	
+					</tbody>
+				</table>
+					 
+    			
+			</div>
+		</div>
+	</div>
+	<br />
+	<br />
+	
+</body>
+<script src = "../js/jquery.js"></script>
+<script src = "../js/bootstrap.js"></script>
+<script src = "../js/jquery.dataTables.js"></script>
+<script src = "../js/dataTables.bootstrap.js"></script>	
+<script type = "text/javascript">
+	function confirmationDelete(anchor){
+		var conf = confirm("Are you sure you want to delete this record?");
+		if(conf){
+			window.location = anchor.attr("href");
+		}
+	} 
+</script>
+
+<script type = "text/javascript">
+	$(document).ready(function(){
+		$("#table").DataTable();
+	});
+</script>
+
 </body>
 </html>
--- payment/index.php
+++ payment/index.php
@@ -1,3 +1,12 @@
+<?php
+  include "../connect.php";
+  session_start();
+  $username = $_SESSION['username'];
+  //new
+  $tripID = $_GET['tripID'];
+  $price = $_GET['price'];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,7 +22,7 @@
 
 <div class="container">
 
-    <form action="paymentProcess.php">
+    <form action="indexnew.php" method="post">
 
         <div class="row">
 
@@ -22,30 +31,30 @@
                 <h3 class="title">billing address</h3>
 
                 <div class="inputBox">
-                    <span>full name :</span>
-                    <input type="text" placeholder="Name" required>
+                    <span>Price :</span>
+                    <input type="text" placeholder="Name" value="<?php echo $price ?>" disabled>
                 </div>
                 <div class="inputBox">
                     <span>email :</span>
-                    <input type="email" placeholder="example@example.com" required>
+                    <input type="email" placeholder="example@example.com" require>
                 </div>
                 <div class="inputBox">
                     <span>address :</span>
-                    <input type="text" placeholder="Address" required>
+                    <input type="text" placeholder="Address" require>
                 </div>
                 <div class="inputBox">
                     <span>city :</span>
-                    <input type="text" placeholder="City" required>
+                    <input type="text" placeholder="City" require>
                 </div>
 
                 <div class="flex">
                     <div class="inputBox">
                         <span>state :</span>
-                        <input type="text" placeholder="State" required>
+                        <input type="text" placeholder="State" require>
                     </div>
                     <div class="inputBox">
                         <span>Post code :</span>
-                        <input type="text" placeholder="Postcode" required>
+                        <input type="text" placeholder="Postcode" require>
                     </div>
                 </div>
 
@@ -61,25 +70,25 @@
                 </div>
                 <div class="inputBox">
                     <span>name on card :</span>
-                    <input type="text" placeholder="Name" required>
+                    <input type="text" placeholder="Name" require>
                 </div>
                 <div class="inputBox">
                     <span>credit card number :</span>
-                    <input type="number" placeholder="1111-2222-3333-4444" required>
+                    <input type="number" placeholder="1111-2222-3333-4444" require>
                 </div>
                 <div class="inputBox">
                     <span>exp month :</span>
-                    <input type="text" placeholder="00/00" required>
+                    <input type="text" placeholder="00/00" require>
                 </div>
 
                 <div class="flex">
                     <div class="inputBox">
                         <span>exp year :</span>
-                        <input type="number" placeholder="2022" required>
+                        <input type="number" placeholder="2022" require>
                     </div>
                     <div class="inputBox">
                         <span>CVV :</span>
-                        <input type="text" placeholder="123" required>
+                        <input type="text" placeholder="123" require>
                     </div>
                 </div>
 
@@ -87,11 +96,13 @@
     
         </div>
 
+        <input type="hidden" name="price" value="<?php echo $price?>">
+        <input type="hidden" name="tripID" value="<?php echo $tripID?>">
         <input type="submit" value="proceed to checkout" class="submit-btn">
 
     </form>
 
 </div>    
-    
+
 </body>
 </html>
